a3bda92bff36157f21cfdbd15b4f40dd14afcc47
cholliclef <clefarahai@gmail.com>
2021-07-29 14:46:01 +0100

    index.php updated

--- index.php
+++ index.php
@@ -13,8 +13,54 @@ require("connect.php");
         <div class="card-header">
             <h2>All Staffs</h2>
             <div>
-                <a href="create.php" class="button_link" title="Add New Record" style="vertical-align:bottom;">Create</a>
-            </div>         
+                <a href="create.php" type="button" class="btn btn-primary" title="Add New Record" style="vertical-align:bottom;">Create</a>
+            </div>
+            <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+example
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <?php if(!empty($message)): ?>
+        <div class="alert alert-success">
+        <?= $message; ?>
+        </div>
+        <?php endif?>
+         <form method="POST">
+            <div class="form-group">
+                <label for="name">Firstname</label>
+                <input type="text" class="form-control" name="firstname" id="firstname" required>
+            </div>
+            <div class="form-group">
+                <label for="name">Surname</label>
+                <input type="text" class="form-control" name="surname" id="surname" required>
+            </div>
+            <div class="form-group">
+                <label for="name">Email</label>
+                <input type="email" class="form-control" name="email" id="email" required>
+            </div>
+            <div class="form-group">
+                <button name="submit" type="submit" value="Create" class="btn btn-info">Create a profile</button>
+            </div>
+         </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>         
         </div>
         <div class="card-body">
             <table class="table table-bordered">
